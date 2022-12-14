4ccaeee3ea4a8332cbfe874ebb88c13796e41aa7
Travis Lavery <travislavery@Traviss-MacBook-Pro.local>
2022-12-14 15:03:32 -0700

    clean up lunch route, add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,28 @@
 import * as dotenv from 'dotenv'
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { landingPage } from './lib/views/landingPage';
 import { yelpFusionSearch } from './lib/yelp_fusion/main';
 
 dotenv.config()
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
   res.send(landingPage())
 })
 
-app.get(['/lunch/:location','/lunch/:location/:category'], async (req, res) => {
-  const lunchOptions = await yelpFusionSearch(req.params)
-  res.json({lunchOptions});
+app.get(['/lunch/:location','/lunch/:location/:category'], (req: Request, res: Response) => {
+  yelpFusionSearch(req.params)
+    .then(data => res.json({data}))
+    .catch(err => {
+      // tslint:disable-next-line:no-console
+      console.error(err)
+      res.status(500).send(err)
+    })
 });
 
 
 app.listen(port, () => {
+  // tslint:disable-next-line:no-console
   return console.log(`Express is listening at http://localhost:${port}`);
 });
